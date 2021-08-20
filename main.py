4800f9f05f79e1c06405735c3943924b6ec3f014
user <gtoshev2@mail.ru>
2021-08-20 21:05:14 +0300

    вроде как все готово

--- main.py
+++ main.py
@@ -18,34 +18,34 @@ data = file.read()
 data_lower = data.lower() # all word to lower
 
 
-def remove_URL(text): #delete all links
-    """Remove URLs from a text string"""
+def remove_URL(text): # remove URLs from text
     return re.sub(r"https?://[^,\s]+,?", "  ", data_lower) #re.sub(r"http://\S+|https://\S+", "", text)
 
-def delete_tags(text):
+def delete_tags(text): # remove all tags from text
     return re.sub(r'<.*?>', ' ', text)
 
-def remove_punctuation(text):
-    # remove all punctuation
+def remove_punctuation(text):    # remove all punctuation from text
     return re.sub(r'[^\w\s]', ' ', text) #re.sub(r'[_,:;#"\'\.!?№><»«]', ' ', text)
 
-def remove_non_relevant (text):
-    return  re.sub(r'[a-z]\S+|[a-z]', ' ', text) #re.sub(r"id\S+|club\S+", "", text)
+def remove_non_relevant (text): # remove non relevant words as eng words
+    return  re.sub(r'[a-z]\S+|[a-z]|_', ' ', text) #re.sub(r"id\S+|club\S+", "", text)
 
-def remove_tags(text):
+def remove_tags(text): #remove tags
     return re.sub(r'<.*?>',' ', text)
 
 
 r_url = remove_URL(data_lower)
-r_tags= remove_tags(r_url)
-r_punctuation= remove_punctuation(r_tags)
+r_tags = remove_tags(r_url)
+r_punctuation = remove_punctuation(r_tags)
 r_non_relevant = remove_non_relevant(r_punctuation)
 
-
-a= word_tokenize(r_non_relevant, language='Russian') # Убераю стоп слова из текста
+a= word_tokenize(r_non_relevant, language='Russian') # remove all stop words from list
 #print(a)
+stop_words = stopwords.words('russian')
+stop_words.append('это') # add new stop words in list
+stop_words.append('который')
 s = []
-en_stops = set(stopwords.words('russian'))
+en_stops = set(stop_words)#set(stopwords.words('russian'))
 for word in a:
     if word not in en_stops:
         s.append(word)
@@ -62,27 +62,21 @@ for word in a:
 # l=[stemmer.stem(word) for word in s]
 # print(l)
 
-#КОД НИЖЕ ВЫПОЛНЯЕТ НЕ КОНТЕКСТНУЮ ЛЕМАНТЕЗАЦИЮ
+# lemantize our text below
 res = []
 morph = pymorphy2.MorphAnalyzer()
 for word in s:
     p = morph.parse(word)[0]
     res.append(p.normal_form)
 #print(res)
+
 counter = Counter(res)
-print(sorted(res, key=counter.get, reverse=True)) # Отсортированные слова по частоте употребления
-
-print(counter.most_common(100))# ВЫВОДИТ ТОП 100 самых популярных слов
-
+#print(sorted(res, key=counter.get, reverse=True)) # sort lits by frequency
+print(counter.most_common(100))# print top 100 words
 
 #ПРИМЕР ТОГО КАК МОЖНО СТОП СЛОВА ВЫТАЩИТЬ ИЗ ТЕКсТА
-# list_of_stop_words = ["в", "и", "по", "за"]
+# list_of_stop_words = ['это', 'который']
+# filtered_str = ' '.join((filter(lambda s: s not in list_of_stop_words, res)))
 #
-# # Строка со стоп-словами
-# string_to_process = "Сервис по поиску работы и сотрудников HeadHunter опубликовал подборку высокооплачиваемых вакансий в России за август."
-#
-# # lambda-функция, фильтрующая стоп-слова
-# split_str = string_to_process.split()
-# filtered_str = ' '.join((filter(lambda s: s not in list_of_stop_words, split_str)))
-#
-# print("Отфильтрованная строка:a", filtered_str)
+#print("Отфильтрованная строка:a", filtered_str)
+
